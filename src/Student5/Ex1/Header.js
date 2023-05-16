db726efc2c6ccec54d949f817d41785b57daa594
Farjana Akter <fakter@xpanxion.com>
2023-05-16 17:59:49 -0400

    update ex1

---
+++ src/Student5/Ex1/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Header = () =>{
+
+    return(
+        <div className = "Main">
+         Dollar Food Store
+         <div>
+        Home|Login|Support|FAQ
+        </div>
+    </div> 
+         
+        
+    )
+}
+
+
+
+
+ 
---
+++ src/Student5/Ex1/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Header} from './Header'
+import './Header.js'
+import './Footer'
+import './Menu'
+
+
+export const Main = () => {
+  return (
+    <div>
+      <Header>  </Header>
+    </div>
+  )
+}
