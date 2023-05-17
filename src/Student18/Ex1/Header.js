002d84f194a77e566ad5b7ecfc469fac9cad559f
xani-dev <xani.gn@gmail.com>
2023-05-17 15:42:33 -0400

    Adding Ex1

---
+++ src/Student18/Ex1/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Nav } from '../../Student0/Ex1/Nav'
+import './header.css';
+
+export const Header = () => {
+	return (
+		<div>
+			<div className='logo'>Dollar Food Store</div>
+			{/* <Nav /> */}
+		</div>
+	);
+};
